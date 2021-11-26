7ea84e7c82d1607a5199da24f5d0d70e29c82f33
Durga69 <90327403+Durga69@users.noreply.github.com>
2021-11-26 23:17:22 +0530

    Create Details.js

---
+++ Details.js
@@ -0,0 +1,26 @@
+let Person1 = {
+    FirstName: 'Durga',
+    LastName: 'Kumar',
+    OfficeAddress: 'NE. Washington, DC 20019'
+}; 
+for( let key in Person1){
+    console.log(key + ": " +Person1[key]);
+}
+
+let Person2 = {
+    FirstName: 'Abhishek',
+    LastName: 'Kumar',
+    OfficeAddress: '46, Mowgli Villas, Chinchwad Bengaluru - 527598'
+}; 
+for( let key in Person2){
+    console.log(key + ": " +Person2[key]);
+}
+ 
+let Person3 = {
+    FirstName: 'Johny',
+    LastName: 'Bhaiya😁',
+    OfficeAddress: '62, Deccan Gymkhana, Patna - 200908'
+}; 
+for( let key in Person3){
+    console.log(key + ": " +Person3[key]);
+}
